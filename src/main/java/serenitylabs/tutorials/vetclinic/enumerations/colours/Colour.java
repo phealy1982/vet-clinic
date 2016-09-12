5f613a24fcd9e37acf9a5611e2c73987bedbb81d
phealy1982 <phealy1982@gmail.com>
2016-09-12 22:28:43 +0100

    attempt two

--- src/main/java/serenitylabs/tutorials/vetclinic/enumerations/colours/Colour.java
+++ src/main/java/serenitylabs/tutorials/vetclinic/enumerations/colours/Colour.java
@@ -1,4 +1,33 @@
 package serenitylabs.tutorials.vetclinic.enumerations.colours;
 
 public enum Colour {
+
+    Red(true), Orange(false), Yellow(true), Green(false),
+    Blue(true), Violet(false), Black(false), White(false);
+
+    private Boolean primary;
+    private Colour opposite;
+
+    static {
+        Red.opposite = Green;
+        Orange.opposite = Violet;
+        Yellow.opposite = Blue;
+        Green.opposite = Red;
+        Blue.opposite = Yellow;
+        Violet.opposite = Orange;
+        Black.opposite = White;
+        White.opposite = Black;
+    }
+
+    Colour(Boolean primary) {
+        this.primary=primary;
+    }
+
+    public Boolean isPrimary() {
+        return primary;
+    }
+
+    public Colour opposite() {
+        return opposite;
+    }
 }
--- src/test/java/serenitylabs/tutorials/vetclinic/enumerations/katas/WhenCalculatingWithColours.java
+++ src/test/java/serenitylabs/tutorials/vetclinic/enumerations/katas/WhenCalculatingWithColours.java
@@ -1,58 +1,53 @@
 package serenitylabs.tutorials.vetclinic.enumerations.katas;
 
 import junitparams.JUnitParamsRunner;
-import junitparams.Parameters;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import serenitylabs.tutorials.vetclinic.enumerations.colours.Colour;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @RunWith(JUnitParamsRunner.class)
 public class WhenCalculatingWithColours {
-    @Test
-    public void should_know_about_all_the_main_colours() {
+
+    @Test public void should_have_all_main_colours() throws Exception {
+
+        List<String> colours = new ArrayList<>();
+
+        for(Colour colour : Colour.values()){
+            colours.add(colour.name());
+        }
+
+        assertThat(colours)
+                .contains("Red", "Orange", "Yellow", "Green", "Blue", "Violet", "Black", "White");
+
     }
 
-    @Test
-    public void should_identify_primary_colours() {
+    @Test public void should_know_primary_colours() throws Exception {
+        assertThat(Colour.Red.isPrimary()).isTrue();
+        assertThat(Colour.Yellow.isPrimary()).isTrue();
+        assertThat(Colour.Blue.isPrimary()).isTrue();
+
     }
 
-    @Test
-    public void should_identify_non_primary_colours() {
+    @Test public void should_know_when_colours_are_not_primary() throws Exception {
+        assertThat(Colour.Orange.isPrimary()).isFalse();
+        assertThat(Colour.Green.isPrimary()).isFalse();
+        assertThat(Colour.Violet.isPrimary()).isFalse();
+        assertThat(Colour.Black.isPrimary()).isFalse();
+        assertThat(Colour.White.isPrimary()).isFalse();
     }
 
-    @Test
-    public void black_and_white_are_not_considered_primary() {
+    @Test public void should_know_each_colours_opposite() throws Exception {
+        assertThat(Colour.Red.opposite()).isEqualTo(Colour.Green);
+        assertThat(Colour.Orange.opposite()).isEqualTo(Colour.Violet);
+        assertThat(Colour.Yellow.opposite()).isEqualTo(Colour.Blue);
+        assertThat(Colour.Green.opposite()).isEqualTo(Colour.Red);
+        assertThat(Colour.Blue.opposite()).isEqualTo(Colour.Yellow);
+        assertThat(Colour.Black.opposite()).isEqualTo(Colour.White);
+        assertThat(Colour.White.opposite()).isEqualTo(Colour.Black);
     }
-
-    @Test
-    public void red_is_the_opposite_of_green() {
-    }
-
-    @Test
-    public void blue_is_the_opposite_of_orange() {
-    }
-
-    @Test
-    public void yellow_is_the_opposite_of_violet() {
-    }
-
-    @Test
-    public void black_is_the_opposite_of_white() {
-    }
-
-    @Test
-    public void opposite_colours_are_symmetric() {
-    }
-
-    /**
-     * This is an example of an alternative approach using JUnitParam
-     */
-    @Parameters({"Red,    Green",
-            "Blue,   Orange",
-            "Violet, Yellow",
-            "Black,  White"})
-    @Test
-    public void should_identify_opposite_colours(Colour colour, Colour expectedOpposite) throws Exception {
-    }
-
 }
