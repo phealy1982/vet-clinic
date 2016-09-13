cf91926d28ab10beee8c819590ec2e3888027b03
phealy1982 <phealy1982@gmail.com>
2016-09-13 22:52:08 +0100

    attempt three

--- src/main/java/serenitylabs/tutorials/vetclinic/enumerations/colours/Colour.java
+++ src/main/java/serenitylabs/tutorials/vetclinic/enumerations/colours/Colour.java
@@ -1,4 +1,33 @@
 package serenitylabs.tutorials.vetclinic.enumerations.colours;
 
 public enum Colour {
+    Red(true), Orange(false), Yellow(true), Green(false), Blue(true),
+    Violet(false), Black(false), White(false);
+
+    private boolean primary;
+    private Colour opposite;
+
+
+    static {
+        Red.opposite = Green;
+        Green.opposite = Red;
+        Black.opposite = White;
+        White.opposite = Black;
+        Blue.opposite = Yellow;
+        Yellow.opposite = Blue;
+        Orange.opposite = Violet;
+        Violet.opposite = Orange;
+    }
+
+    Colour(boolean primary) {
+        this.primary = primary;
+    }
+
+    public boolean isPrimary() {
+        return primary;
+    }
+
+    public Colour opposite() {
+        return opposite;
+    }
 }
--- src/test/java/serenitylabs/tutorials/vetclinic/enumerations/katas/WhenCalculatingWithColours.java
+++ src/test/java/serenitylabs/tutorials/vetclinic/enumerations/katas/WhenCalculatingWithColours.java
@@ -6,53 +6,55 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import serenitylabs.tutorials.vetclinic.enumerations.colours.Colour;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @RunWith(JUnitParamsRunner.class)
 public class WhenCalculatingWithColours {
     @Test
     public void should_know_about_all_the_main_colours() {
+        List<String> colours = new ArrayList<>();
+
+        for(Colour colour : Colour.values()){
+            colours.add(colour.name());
+        }
+
+        assertThat(colours).contains("Red", "Orange", "Yellow", "Green", "Blue",
+            "Violet", "Black", "White");
     }
 
     @Test
-    public void should_identify_primary_colours() {
+    @Parameters({"Red", "Yellow", "Blue"})
+    public void should_identify_primary_colours(Colour colour) {
+        assertThat(colour.isPrimary()).isTrue();
     }
 
     @Test
-    public void should_identify_non_primary_colours() {
+    @Parameters({"Orange", "Violet", "Black", "White", "Green"})
+    public void should_identify_non_primary_colours(Colour colour) {
+        assertThat(colour.isPrimary()).isFalse();
     }
 
     @Test
-    public void black_and_white_are_not_considered_primary() {
+    @Parameters({"Red",    "Green",
+        "Blue",   "Yellow",
+        "Violet", "Orange",
+        "Black",  "White"})
+    public void opposite_colours_are_symmetric(Colour colour) {
+            assertThat(colour.opposite().opposite()).isEqualTo(colour);
     }
+
 
     @Test
-    public void red_is_the_opposite_of_green() {
-    }
-
-    @Test
-    public void blue_is_the_opposite_of_orange() {
-    }
-
-    @Test
-    public void yellow_is_the_opposite_of_violet() {
-    }
-
-    @Test
-    public void black_is_the_opposite_of_white() {
-    }
-
-    @Test
-    public void opposite_colours_are_symmetric() {
-    }
-
-    /**
-     * This is an example of an alternative approach using JUnitParam
-     */
     @Parameters({"Red,    Green",
-            "Blue,   Orange",
-            "Violet, Yellow",
-            "Black,  White"})
-    @Test
+        "Blue,   Yellow",
+        "Violet, Orange",
+        "Black,  White"})
     public void should_identify_opposite_colours(Colour colour, Colour expectedOpposite) throws Exception {
+        assertThat(colour.opposite()).isEqualTo(expectedOpposite);
+        assertThat(expectedOpposite.opposite()).isEqualTo(colour);
     }
 
 }
